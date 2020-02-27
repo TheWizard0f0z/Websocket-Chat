3e78899cddf90091122f378d3b9f28fadef9a805
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2020-02-27 20:12:09 +0100

    fix disconnect event v3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const socket = require('socket.io');
 
 const messages = [];
-const users = [];
+let users = [];
 
 app.use(express.static(path.join(__dirname + '/client')));
 
@@ -32,18 +32,18 @@ io.on('connection', socket => {
 
   socket.on('message', message => {
     console.log("Oh, I've got something from " + socket.id);
+
     messages.push(message);
+
     socket.broadcast.emit('message', message);
   });
 
   socket.on('disconnect', () => {
     console.log('Oh, socket ' + socket.id + ' has left');
 
-    removeUser = () => {
-      users.filter(user => user.id !== socket.id);
-    };
-
     const leavingUser = users.find(user => user.id === socket.id);
+
+    users = users.filter(user => user.id !== socket.id);
 
     socket.broadcast.emit('leave', leavingUser.name);
   });
